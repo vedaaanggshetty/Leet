eedb4b03280300b6c8273f396d3249cc0db4dcec
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2024-10-04 18:51:22 +0530

    Time: 719 ms (82.75%), Space: 20 MB (60.36%) - LeetHub

---
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def construct2DArray(self, original, m, n):
+       
+      if m * n != len(original):
+        return []
+      
+      ans = []
+      
+      for i in range(m):  #why because you are looping through the columns
+      # and then appending those chunks as rows..
+        ans.append(original[i*n :(i+1)*n])
+      return ans
+
+      if m and n <= 1:
+        return []

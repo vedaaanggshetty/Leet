379d5196ca35e4d382138584ef994f4937ab533b
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2024-10-09 06:03:49 +0530

    Time: 31 ms (65.72%), Space: 11.7 MB (64.59%) - LeetHub

---
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isAcronym(self, words, s):
+        acr = []
+        for word in words:
+            acr.append(word[0])  # abc
+        acronym = ''.join(acr)  # becomes "abc"
+        if acronym == s:
+            return True
+        return False

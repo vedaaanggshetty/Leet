3b3f37a1da4c04ec40fe2b5faa0d33484f3d8535
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2024-10-22 09:29:04 +0530

    Time: 0 ms (100%), Space: 11.5 MB (93.67%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -0,0 +1,47 @@
+class Solution(object):
+    def missingInteger(self, nums):
+        # TAKE ONE
+        # nums.sort()
+        # summ = 0
+        # for s in nums:
+        #     summ += s
+        # n = len(nums)
+        # otherSum = n * (n+1)/2 + n/2
+        # total = 0
+        # total = otherSum - summ
+        # return total
+        
+
+        # TAKE TWO
+        # nums.sort()
+        # curr = 0
+        # maxSum = float('-inf')
+        # for num in nums:
+        #     curr += num
+        #     if curr > maxSum:
+        #         maxSum = curr       
+        
+        # return maxSum
+
+        n = len(nums)
+        seq = nums[0]
+        last = 0
+
+        for i in range(1,n):
+            if nums[i] == nums[i-1] + 1:
+                seq += nums[i]
+                last  = i
+            else:
+                break
+        
+
+        missing = seq
+        numSet = set(nums)
+
+        while missing in numSet:
+            missing += 1
+        
+        return missing
+           
+          
+        

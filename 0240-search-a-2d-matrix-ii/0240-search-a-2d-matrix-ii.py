e8d74ea3be228edce9f41899bc379288319e1cd2
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2025-01-09 08:39:11 +0530

    Time: 135 ms (15.09%), Space: 18.6 MB (20.52%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,17 +1,19 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
-        m=len(matrix)
-        n=len(matrix[0])
-        
-        i=m-1
-        j=0
-        
-        while i>=0 and j<n:
-            if matrix[i][j]==target:
+        # brute force approach
+
+        # cant use binary search because row and column arent contiguously arranged in ascending order
+        row = len(matrix)
+        col = len(matrix[0])
+
+        i = row - 1
+        j = 0
+
+        while j<col and i >= 0:
+            if matrix[i][j] == target:
                 return True
-            elif matrix[i][j]<target:
-                j+=1
-            else:
-                i-=1
-                
+            if matrix[i][j] < target:
+                j += 1
+            else: 
+                i -= 1
         return False

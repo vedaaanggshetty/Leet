b03f0831dac57c221bf0d2a3b1cfaa916d20eaec
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2025-01-14 13:27:17 +0530

    Time: 0 ms (100%), Space: 12.4 MB (27.04%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -6,10 +6,15 @@ class Solution(object):
         for i in range(1,n):
             if nums[i-1] > nums[i]:
                 count += 1
-        # 
-        for i in range(1,n):
-            if num[n-1] > nums[0]:
-                count += 1
+        # check if last element is grete thwn the first element
+        if nums[n-1] > nums[0]:
+            count += 1
         
         # return true if violations are less than or equal to 1
         return count <= 1
+
+        # -------------- another approach ----------------
+        # for i in range(1, n):
+            # if (nums[i] > (nums[i+1] %n)):
+                # count += 1
+                # return count <= 1

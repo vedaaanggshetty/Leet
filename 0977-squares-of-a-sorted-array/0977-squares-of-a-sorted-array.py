bbb0799078194e4b2b96be669b6bee9114162fc8
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2025-01-28 06:00:08 +0530

    Time: 5 ms (92.13%), Space: 14.3 MB (52.62%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -5,3 +5,8 @@ class Solution(object):
             sq.append(i**2)
         sorArr = sorted(sq)
         return sorArr
+
+    # else you can do
+        # sq = [i**2 for i in nums]
+        # sortedA = sorted(sq)
+        # return sortedA

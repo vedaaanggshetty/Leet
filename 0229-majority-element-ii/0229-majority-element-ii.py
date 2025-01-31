5c6c18be933b41743b6e779b3d7100e470a4a9a8
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2025-01-31 11:58:31 +0530

    Time: 4 ms (77.53%), Space: 13 MB (47.78%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,12 +1,21 @@
 # import Counter
 class Solution(object):
     def majorityElement(self, nums):
-        # import counter
-        res =[]
+        # # import counter
+        # res =[]
+        # n = len(nums)
+        # freq = Counter(nums)
+        # # you can enumerate using dict
+        # for k,v in freq.items():
+        #     if v > n/3:
+        #         res.append(k)
+        # return res
+
+    # ------------  better efficiency ----------
+        res = []
         n = len(nums)
-        freq = Counter(nums)
-        # you can enumerate using dict
-        for k,v in freq.items():
-            if v > n/3:
-                res.append(k)
+        freq = n // 3
+        for i in set(nums):
+            if (nums.count(i) > freq):
+                res.append(i)
         return res

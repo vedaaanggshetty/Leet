5f34a61db0a40f8a2c18553336f141eb71c73880
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2025-01-31 14:51:42 +0530

    Time: 3 ms (38.14%), Space: 12.4 MB (40.17%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -5,6 +5,6 @@ class Solution(object):
         for i in range(n):
             for j in range(i+1, n):
                 if(nums[i] == nums[j]):
-                    j += 1
+                    # j += 1
                     c += 1
         return c

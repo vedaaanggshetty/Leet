736eb55e242f29feade435cecc0d04957398d532
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2025-01-31 15:25:32 +0530

    Time: 47 ms (10.97%), Space: 12.7 MB (37.48%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        numLen = len(nums)
+        # if numLen % 2 != 0:
+        #     continue
+        j = n
+        # res = [nums[0]]
+        res=[]
+        for i in range(numLen):
+            res.append(nums[i])
+            res.append(nums[j])
+            if(j==numLen-1):
+                break
+            j=j+1
+        return res

51aee28771b67816831c1468ae060b003763c8c7
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2025-04-19 17:41:46 +0530

    Time: 17 ms (72.03%), Space: 12.7 MB (67.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,32 @@
 class Solution(object):
     def isAnagram(self, s, t):
+        # if len(s) != len(t):
+        #     return False
+        # map = {}
+        # for c in s:
+        #     if c in map:
+        #         map[c] += 1
+        #     else:
+        #         map[c] = 1
+        #     map[c] = map.get(c,0) + 1
+        # for c in t:
+        #     if c not in map:
+        #         return False
+        #     map[c] -= 1
+
+        # for c in map.values():
+        #     if c != 0:
+        #         return False
+        # return True
         
         char = [0] * 26
-        for i in s:
-            char[ord(i) - ord('a')]+=1
-        
-        for j in t:
-            char[ord(j)- ord('a')]-= 1
-        
-        for k in char:
-            if k != 0:
+        for c in s:
+            char[ord(c) - ord('a')] += 1
+
+        for c in t:
+            char[ord(c) - ord('a')] -= 1
+
+        for i in char:
+            if i!=0:
                 return False
         return True

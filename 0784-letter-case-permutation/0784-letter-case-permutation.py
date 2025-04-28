c33a5385da119c87d1fb90aa94d0e6d0ed65a58e
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2025-04-28 20:29:54 +0530

    Time: 7 ms (71.58%), Space: 13.6 MB (36.3%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -1,14 +1,27 @@
 class Solution(object):
     def letterCasePermutation(self, s):
-        res = [""]
-        for c in s:
-            temp  = []
-            if c.isalpha():
-                for r in res:
-                    temp.append(r+c.lower())
-                    temp.append(r+c.upper())
-            else:
-                for r in res:
-                    temp.append(r+c)
-            res = temp
+        # res = [""]
+        # for c in s:
+        #     temp = []
+        #     for r in res:
+        #         if c.isalpha():
+        #             temp.append(r + c.lower())
+        #             temp.append(r + c.upper())
+        #         else:
+        #             temp.append(r + c)
+        #     res = temp
+        # return res
+
+        #  BT
+        res = []
+        def BT(sub="",i=0):
+            if(len(s)== len(sub)):
+                res.append(sub)
+                return 
+            
+            if(s[i].isalpha()):
+                BT(sub + s[i].swapcase(), i+1)
+            BT(sub + s[i], i+1)
+
+        BT()
         return res

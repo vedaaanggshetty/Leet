ff3cea417ec57e87c31e9b81c5567211e8666a6d
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2025-04-28 20:23:38 +0530

    Time: 6 ms (73.97%) | Memory: 12.9 MB (91.78%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -2,13 +2,12 @@ class Solution(object):
     def letterCasePermutation(self, s):
         res = [""]
         for c in s:
-            temp  = []
-            if c.isalpha():
-                for r in res:
-                    temp.append(r+c.lower())
-                    temp.append(r+c.upper())
-            else:
-                for r in res:
-                    temp.append(r+c)
+            temp = []
+            for r in res:
+                if c.isalpha():
+                    temp.append(r + c.lower())
+                    temp.append(r + c.upper())
+                else:
+                    temp.append(r + c)
             res = temp
         return res

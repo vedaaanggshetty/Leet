3c07b9cab222d24482ca37588b25502f43117d1b
Vedang <142001559+vedaaanggshetty@users.noreply.github.com>
2025-06-22 22:49:53 +0530

    Time: 19 ms (7.10%) | Memory: 17 MB (99.87%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -7,18 +7,47 @@
 class Solution(object):
     def diameterOfBinaryTree(self, root):
         # recursive
-        self.dia = 0
+        # self.dia = 0
 
-        def BT(root):
-            if not root:
-                return False
+        # def BT(root):
+        #     if not root:
+        #         return False
             
-            leftH =  BT(root.left)
-            rightH = BT(root.right)
+        #     leftH =  BT(root.left)
+        #     rightH = BT(root.right)
 
           
-            self.dia = max(self.dia, leftH + rightH)
-            return 1 + max(leftH, rightH)
-        BT(root)
-        return self.dia
+        #     self.dia = max(self.dia, leftH + rightH)
+        #     return 1 + max(leftH, rightH)
+        # BT(root)
+        # return self.dia
         
+        # Iterative
+        s = [(root, False)]
+        dia = 0
+        maxx = {}
+        while s:
+            n,v = s.pop()
+
+            if not v:
+                s.append((n, True))
+                if n.left:
+                    s.append((n.left, False))
+                if n.right:
+                    s.append((n.right, False))
+            
+            else:
+                if not n.left:
+                    leftH = 0
+                else:
+                    leftH = maxx.pop(n.left)
+                
+                if not n.right:
+                    rightH = 0
+                else:
+                    rightH = maxx.pop(n.right)
+
+                dia = max(dia, leftH + rightH)
+                maxx[n] = max(leftH, rightH) + 1
+        return dia
+                

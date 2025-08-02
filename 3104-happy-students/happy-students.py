4948db45a073df932cbac758642d02b2c4c900da
Shetty Vedanga <142001559+vedaaanggshetty@users.noreply.github.com>
2025-08-02 09:38:01 +0530

    Time: 60 ms (92.86%) | Memory: 20.1 MB (50.00%) - LeetSync

--- 3104-happy-students/happy-students.py
+++ 3104-happy-students/happy-students.py
@@ -13,4 +13,12 @@ class Solution(object):
             if nums[i]<s and nums[i+1]>s:
                 res+=1
         return res
-        
+
+        # n = len(nums)
+        # nums.sort()
+        # res = 0
+        # for i in range(n+1):
+        #     c = sum(1 for num in nums if num < i)
+        #     if c >= i and i not in nums:
+        #         res += 1
+        # return res

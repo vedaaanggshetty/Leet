5df011107c35e0d9c07296446d2624b382bb1b34
Shetty Vedanga <142001559+vedaaanggshetty@users.noreply.github.com>
2025-08-05 07:47:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (56.41%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def generate(self, numRows):
+        res = []
+        # n = len(numRows)
+        for i in range(numRows):
+            row = [1] * (i+1)
+
+            for j in range(1, i):
+                row[j] = res[i-1][j-1] + res[i-1][j]
+
+            res.append(row)
+        return res

c202a9433e8c101a9b476c42b72fbcd131ce4c1e
Shetty Vedanga <142001559+vedaaanggshetty@users.noreply.github.com>
2025-08-08 16:56:25 +0530

    Time: 76 ms (29.41%) | Memory: 12.3 MB (98.78%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -5,8 +5,25 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
+        if "0" in [num1,num2]:
+            return "0"
 
-        n1 = int(num1)
-        n2 = int(num2)
+        res = [0] * (len(num1) + len(num2))
+        for i in range(len(num1)-1,-1,-1):
+            for j in range(len(num2)-1,-1,-1):
+                mul = (ord(num1[i]) -ord('0')) * (ord(num2[j])-ord('0'))
+                summ = mul + res[i+j+1]
+                res[i+j+1] = summ % 10
+                res[i+j] += summ//10
+
+        res = ''.join(map(str,res))
+        return res.lstrip('0')
+            
+
+
+
+# forbidden usage
+        # n1 = int(num1)
+        # n2 = int(num2)
         
-        return str(n1 * n2)
+        # return str(n1 * n2)

35b9aa0c24261b1db723010f32241a19362984e6
Shetty Vedanga <142001559+vedaaanggshetty@users.noreply.github.com>
2025-08-10 21:18:08 +0530

    Time: 5 ms (87.5%), Space: 12.5 MB (95%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        string = "abc"
+        # if string in s:
+        #     return True
+        # return False
+
+
+        # st = []
+        # for c in s:
+        #     st.append(c)
+        #     if len(st) >= 3 and st[-3:] == ["a", "b", "c"]:
+        #         st.pop()
+        #         st.pop()
+        #         st.pop()
+        # return len(st) == 0
+
+        while string in s:
+            s = s.replace(string, "")
+        return not s

6201c3df2329e888e554dfe4f8d802e8bbabd065
Shetty Vedanga <142001559+vedaaanggshetty@users.noreply.github.com>
2025-09-01 00:29:32 +0530

    Time: 3330 ms (8.52%), Space: 12.6 MB (14.07%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -0,0 +1,39 @@
+class Solution:
+    def solveSudoku(self, board):
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empty = []
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == ".":
+                    empty.append((r, c))
+                else:
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    boxes[(r // 3) * 3 + (c // 3)].add(val)
+
+        def backtrack(i):
+            if i == len(empty):
+                return True
+            r, c = empty[i]
+            b = (r // 3) * 3 + (c // 3)
+            for ch in map(str, range(1, 10)):
+                if ch not in rows[r] and ch not in cols[c] and ch not in boxes[b]:
+                    board[r][c] = ch
+                    rows[r].add(ch)
+                    cols[c].add(ch)
+                    boxes[b].add(ch)
+
+                    if backtrack(i + 1):
+                        return True
+
+                    board[r][c] = "."
+                    rows[r].remove(ch)
+                    cols[c].remove(ch)
+                    boxes[b].remove(ch)
+            return False
+
+        backtrack(0)

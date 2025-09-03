52348a264d39e84018b229638c5ce35c8a5a1c36
Shetty Vedanga <142001559+vedaaanggshetty@users.noreply.github.com>
2025-09-03 09:32:57 +0530

    Time: 14 ms (77.4%), Space: 16.1 MB (65.22%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,12 +1,24 @@
 class Solution(object):
     def findJudge(self, n, trust):
-        # so no cycle? in a DAG?
-        count = [0] * (n+1)
-        for i, j in trust:
-            count[i] -= 1
-            count[j] += 1 
+        # # so no cycle? in a DAG?
+        # count = [0] * (n+1)
+        # for i, j in trust:
+        #     count[i] -= 1
+        #     count[j] += 1 
+
+        # for i in range(1, n+1):
+        #     if count[i] == n - 1:
+        #         return i
+        # return -1
+
+        trusted = [0] * (n + 1)
+        trusts = [False] * (n + 1)
+
+        for u,v in trust:
+            trusted[v] += 1
+            trusts[u] = True
 
         for i in range(1, n+1):
-            if count[i] == n - 1:
+            if trusted[i] == n - 1 and not trusts[i]:
                 return i
         return -1

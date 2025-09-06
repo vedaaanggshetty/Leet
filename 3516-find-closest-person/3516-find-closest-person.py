4fe5de05e44b59d81cd73b3f40ddc08a18a494dd
Shetty Vedanga <142001559+vedaaanggshetty@users.noreply.github.com>
2025-09-06 18:37:33 +0530

    Time: 0 ms (100%), Space: 12.4 MB (84.07%) - LeetHub

--- 3516-find-closest-person/3516-find-closest-person.py
+++ 3516-find-closest-person/3516-find-closest-person.py
@@ -2,11 +2,11 @@ class Solution(object):
     def findClosest(self, x, y, z):
         if abs(x-z) == abs(y-z):
             return 0
-        elif abs(x-z) > abs(y-z):
-            return 2
-        else:
-            return 1
+        # elif abs(x-z) > abs(y-z):
+        #     return 2
+        # else:
+        #     return 1
         
-        # return 2 if abs(x-z) > abs(y-z) else 1
+        return 2 if abs(x-z) > abs(y-z) else 1
 
         

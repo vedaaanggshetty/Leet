39433680debbffe68f749939232dcd78f0904a41
Shetty Vedanga <142001559+vedaaanggshetty@users.noreply.github.com>
2025-09-09 06:12:26 +0530

    Time: 2 ms (56.84%) | Memory: 12.5 MB (30.09%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def numDecodings(self, s):
+        # mapp = {chr(i) : str(i - 64) for i in range(65,91)}
+        # print(mapp)
+
+        # c = 0
+        # for i in range(len(s)):
+        #     if s[i] in mapp.values():
+        #         c += 1
+        #     elif s[:i] == '0':
+        #         return 0
+        # return c
+        if s[0] == '0' or not s:
+            return 0
+
+        n = len(s)
+        dp = [0] * (n + 1)
+
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            one = int(s[i-1])
+            two = int(s[i-2:i])
+
+            if 1 <= one <= 9:
+                dp[i] += dp[i-1]
+            if 10 <= two  <= 26:
+                dp[i] += dp[i-2]
+
+        return dp[n]
+

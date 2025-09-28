7bef436bd2253728dc48b22893c1a7e6b5903824
Shetty Vedanga <142001559+vedaaanggshetty@users.noreply.github.com>
2025-09-28 16:47:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (74.69%) - LeetSync

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sumZero(self, n):
+        res = []
+        for i in range(1, n//2 + 1):
+            res.append(i)
+            res.append(-i)
+
+        if n % 2 == 1:
+            res.append(0)
+
+        return res

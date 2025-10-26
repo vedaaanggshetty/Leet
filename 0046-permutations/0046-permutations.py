f1eb8e5296e43f9cf0e7c9bc61de85765604e377
Shetty Vedanga <142001559+vedaaanggshetty@users.noreply.github.com>
2025-10-26 12:10:55 +0530

    Time: 4 ms (27.97%), Space: 12.7 MB (53.23%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,13 +1,19 @@
 class Solution(object):
     def permute(self, nums):
-        def BT(s, e):
-            if s == e:
-                res.append(nums[:])
-                return
-            for i in range(s,e):
-                nums[s], nums[i] = nums[i], nums[s]
-                BT(s+1, e)
-                nums[s], nums[i] = nums[i], nums[s]
         res = []
-        BT(0, len(nums))
+
+        def BT(p, arr):
+            if len(p) == len(nums):
+                res.append(p[:])
+                return
+
+            for i in range(len(nums)):
+                if not arr[i]:
+                    arr[i] = True
+                    p.append(nums[i])
+                    BT(p, arr)           
+                    p.pop()
+                    arr[i] = False
+
+        BT([], [False] * len(nums))
         return res
